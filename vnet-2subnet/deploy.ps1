0dccbb3a273c7a38b55e56397d7995ddb98e0404
amer <cloud.amer@outlook.com>
2021-03-28 19:53:35 +0500

    updated

--- vnet-2subnet/deploy.ps1
+++
@@ -1,8 +0,0 @@
-$resourceGroupName = Read-Host -Prompt "Enter the name of the resource group to be created"
-$location = Read-Host -Prompt "Enter the location (i.e. centralus)"
-
-New-AzResourceGroup -Name $resourceGroupName -Location $location
-
-New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri ""
-
-Write-Host ""
